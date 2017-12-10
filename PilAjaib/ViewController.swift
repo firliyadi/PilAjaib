c6fabd4e38862cf0afc3c183988477378d56413b
firliyadi <afirliyadi@gmail.com>
2017-12-11 03:10:44 +0700

    UIPickerViewDataSource, UIPickerViewDelegate
    Learn protocol : UIPickerViewDataSource and UIPickerViewDelegate

--- PilAjaib/ViewController.swift
+++ PilAjaib/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    let prov = ["Denpasar", "Jakarta", "Jawa Barat", "Jawa Tengah", "Jawa Timur", "Sumatera Barat", "Sumatera Selatan"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return prov.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return prov[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(prov[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 }
 
